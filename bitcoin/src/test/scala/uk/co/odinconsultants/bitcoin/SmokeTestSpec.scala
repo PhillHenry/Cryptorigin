0dafa16b775783d79be68c28984753e844b6cfd1
PhillHenry <phillhenry@gmail.com>
2017-07-17 10:11:12 +0100

    Bitcoin module created

---
+++ bitcoin/src/test/scala/uk/co/odinconsultants/bitcoin/SmokeTestSpec.scala
@@ -0,0 +1,13 @@
+package uk.co.odinconsultants.bitcoin
+
+import org.scalatest.{Matchers, WordSpec}
+
+class SmokeTestSpec extends WordSpec with Matchers {
+
+  "The project" should {
+    "be set up correctly" in {
+      println("Seems to be working. Delete this class when proper work begins on this project.")
+    }
+  }
+
+}
