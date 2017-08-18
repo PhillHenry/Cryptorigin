83a75270807c3877a4f20078e54896aaab7645ee
PhillHenry <phillhenry@gmail.com>
2017-08-18 08:33:40 +0100

    Problem in production. Added debugging. Also, 66 bytes of address can happen.

--- bitcoin/src/main/scala/uk/co/odinconsultants/bitcoin/parsing/DomainOps.scala
+++ bitcoin/src/main/scala/uk/co/odinconsultants/bitcoin/parsing/DomainOps.scala
@@ -1,14 +1,16 @@
 package uk.co.odinconsultants.bitcoin.parsing
 
+import org.bitcoinj.core.ScriptException
 import org.bitcoinj.core.Utils.sha256hash160
 import org.bitcoinj.script.Script
 import org.zuinnote.hadoop.bitcoin.format.common.BitcoinUtil.getTransactionHash
 import org.zuinnote.hadoop.bitcoin.format.common._
+import uk.co.odinconsultants.bitcoin.core.Logging
 import uk.co.odinconsultants.bitcoin.parsing.Indexer.{BackReference, PubKey, networkParams}
 
 import scala.collection.JavaConversions._
 
-object DomainOps {
+object DomainOps extends Logging {
 
   val toTransactions: (BitcoinBlock) => Seq[BitcoinTransaction] = _.getTransactions
 
@@ -27,15 +29,24 @@ object DomainOps {
     toPublicKey(bytes)
   }
 
+  /**
+    * @see http://codesuppository.blogspot.co.at/2014/01/how-to-parse-bitcoin-blockchain.html
+    */
   def toPublicKey(bytes: Array[Byte]): Option[Array[Byte]] = {
     if (bytes.length == 67 && bytes(0) == 65) {
-      Some(sha256hash160(bytes.tail))
-    } else {
+      Some(sha256hash160(bytes.tail.take(65)))
+    } else if (bytes.length == 66) {
+      Some(sha256hash160(bytes.take(65)))
+    } else try {
       val script = new Script(bytes)
 
       if (script.isSentToAddress) Some(script.getToAddress(networkParams).getHash160)
       else if (script.isPayToScriptHash) Some(script.getToAddress(networkParams).getHash160)
       else None
+    } catch {
+      case x: ScriptException =>
+        error(s"Could not convert script of length ${bytes.length}. Bytes (as hex) are ${org.apache.commons.codec.binary.Hex.encodeHex(bytes)}")
+      throw x
     }
   }
 
